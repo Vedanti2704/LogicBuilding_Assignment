7b7a17cf2db9cb62b530e6d3be4a33b89a8d34bf
Vedanti Patil <vedantivpatil27@gmail.com>
2026-06-06 22:51:40 +0530

    Update 5_que24.c

--- 5_que24.c
+++ 5_que24.c
@@ -1,44 +1,30 @@
-/*write a program which accept number from user and display its table
+/* write a program which accept number from user and print its number line
+Input : 8
+Output : -4 -3 -2 -1 0 1 2 3 4 
 
-Input : 2
-Output :2 4 6 8 10 12 14 16 18 20
-
-Input : 5
-Output :5 10 15 20 25 30 35 40 45 50 
-
-Input : -5
-Output :5 10 15 20 25 30 35 40 45 50
 */
 
-#include <stdio.h>
+#include <stdio.h.>
 
-void Table(int iNo)
+void Display(int iNo)
 {
+    
     int iCnt = 0;
 
-     if(iNo < 0)
+    for(iCnt = -iNo; iNo >= iCnt; iCnt++)
     {
-        iNo = -iNo;
+        printf("%d\t" ,iCnt);
+
     }
-
-
-    for(iCnt = 1; iCnt <=10 ; iCnt++ )
-    {
-        printf("%d\t",iNo * iCnt);
-    }
-
 }
-
-
 int main()
 {
     int iValue = 0;
 
-    printf("Enter number : ");
-    scanf("%d", &iValue);
+    printf("Enter number : \n");
+    scanf("%d",&iValue);
 
-    Table(iValue);
-    
+    Display(iValue);
 
     return 0;
 }
