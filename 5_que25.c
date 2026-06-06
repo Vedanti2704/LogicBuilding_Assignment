f1e0e9d15f67de18fd7ca7a59b6a97f2ec2cea6d
Vedanti Patil <vedantivpatil27@gmail.com>
2026-06-06 22:52:58 +0530

    Update 5_que25.c

--- 5_que25.c
+++ 5_que25.c
@@ -1,44 +1,33 @@
-/*write a program which accept number from user and display its table in reverse order
+/* write a program which accept N number from user and print first 5 multiple of N
+Input : 4
+Output : 4 8 12 16 20 
 
-Input : 2
-Output :20 18 16 14 12 10 8 6 4  2  
-
-Input : 5
-Output : 50 45 40 35 30 25 20 15 10 5
-
-Input : -5
-Output :  50 45 40 35 30 25 20 15 10 5
 */
 
-#include <stdio.h>
+#include <stdio.h.>
 
-void TableRev(int iNo)
+void MultipleDisplay(int iNo)
 {
-    int iCnt = 0;
     
-     if(iNo < 0)
-    {
-        iNo = -iNo;
-    }
+    int iCnt = 0;
 
-
-    for(iCnt = 10; iCnt >=1 ; iCnt-- )
+    for(iCnt = 1; iCnt <= 5; iCnt++)
     {
-        printf("%d\t",iNo * iCnt);
+            printf("%d\t", iNo * iCnt);
+       
+
     }
+    
 
 }
-
-
 int main()
 {
     int iValue = 0;
 
-    printf("Enter number : ");
-    scanf("%d", &iValue);
+    printf("Enter number : \n");
+    scanf("%d",&iValue);
 
-    TableRev(iValue);
-    
+    MultipleDisplay(iValue);
 
     return 0;
 }
