d71368836f9e3281e9c9f52fd954c55b5e1cbb79
Vedanti Patil <vedantivpatil27@gmail.com>
2026-06-15 21:48:04 +0530

    Add files via upload

---
+++ que109.c
@@ -0,0 +1,32 @@
+/* Accept number from user and display bellow pattern
+Input : 5
+Output : # 1 * #   2 * #  3 * #  4 * 
+*/
+
+#include<stdio.h>
+
+void Pattern(int iNo)
+{
+    int iCnt = 0;
+    
+    for(iCnt = 1;iCnt <= iNo; iCnt++)
+    {
+        printf(" # ");
+        printf("  %d ", iCnt );
+        printf("  * ");
+    }
+
+    
+
+}
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number of elements : \n");
+    scanf("%d",&iValue);
+
+    Pattern(iValue);
+
+    return 0;
+}
